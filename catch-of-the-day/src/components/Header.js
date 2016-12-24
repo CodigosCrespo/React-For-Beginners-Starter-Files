820c0c883e6f8860fa2ba4a051f170c5ec092909
Jerel Crespo <mrc220@live.com>
2016-12-24 16:43:01 -0500

    syncing 8th vid file changes

--- catch-of-the-day/src/components/Header.js
+++ catch-of-the-day/src/components/Header.js
@@ -1,7 +1,6 @@
 import React from 'react';
 
-class Header extends React.Component {
-  render() {
+const Header = (props) => {
     return (
       <header className="top">
         <h1>
@@ -11,10 +10,13 @@ class Header extends React.Component {
           <span className="the">the</span>
         </span>
         Day</h1>
-        <h3 className="tagline"><span>{this.props.tagline}</span></h3>
+        <h3 className="tagline"><span>{props.tagline}</span></h3>
       </header>
     )
-  }
 }
+
+
+
+
 
 export default Header;
