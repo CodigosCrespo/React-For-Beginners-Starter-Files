f306f16b78a419c4b5e4f2c833a9394dc471b2ef
Jerel Crespo <mrc220@live.com>
2016-12-24 15:00:36 -0500

    syncing 4th vid file changes

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -2,7 +2,14 @@ import React from 'react';
 
 class StorePicker extends React.Component {
   render() {
-    return <p>Hello</p>
+    return (
+        <form className="store-selector">
+          <h2>Pleae Enter A Store</h2>
+          <input type="text" required placeholder="Store Name" />
+          <button type="submit">Visit Store ➥</button>
+        { /* javascript curly braces, then comment as javascript.. will also count as a parent in JSX */}
+        </form>
+      )
   }
 }
 
