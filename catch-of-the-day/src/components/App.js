70683c4d62fe89b3bca15f730419da12012ee8c4
Jerel Crespo <mrc220@live.com>
2016-12-29 10:13:41 -0500

    syncing 16th: updating order state

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -8,10 +8,11 @@ import Fish from './Fish';
 class App extends React.Component {
   constructor () {
     super();
-
+    // binding .this to methods the react way
     this.addFish = this.addFish.bind(this);
     this.loadSamples = this.loadSamples.bind(this);
-    // getinitialState
+    this.addToOrder = this.addToOrder.bind(this);
+    // getinitialState, where we will update our virtualDOM
     this.state = {
       fishes: {},
       order: {}
@@ -20,12 +21,12 @@ class App extends React.Component {
 
   addFish(fish) {
     // update our state
-    const fishes = {...this.state.fishes};
+    const fishes = {...this.state.fishes}; // object spread, taking existing copy of fishes state
     // add in our new fish
     const timestamp = Date.now();
     fishes[`fish-${timestamp}`] = fish;
     // set state
-    this.setState({ fishes })
+    this.setState({ fishes }) // short for ({ fishes: fishes }) -> sets fishes state to new state
   }
 
   loadSamples() {
@@ -34,17 +35,28 @@ class App extends React.Component {
     })
   }
 
+  addToOrder(key) {
+    // take a copy of our state
+    const order = {...this.state.order}; // object spread, taking existing copy of order state
+    // update or add the new number of fish ordered
+    order[key] = order[key] + 1 || 1; // adding to existing OR setting to 1 (new order of fishes key)
+    // update our state
+    this.setState({ order }) // this.setState({order:order}) -> sets order const to new state
+    // decrement order ? (order[key] = order[key] - 1 || 0;)
+  }
+
   render() {
     return (
       <div className="catch-of-the-day">
         <div className="menu">
           <Header tagline="Fresh Seafood Market" />
           <ul className="list-of-fishes">
-            {
+            { // this is beautiful T.T
               Object
               .keys(this.state.fishes)
-              .map(key => <Fish key={key} details={this.state.fishes[key]}/>)
+              .map(key => <Fish key={key} index={key} details={this.state.fishes[key]} addToOrder={this.addToOrder} />)
             }
+          {/* key is for react, index is for us to pass down via props */}
           </ul>
         </div>
         <Order />
