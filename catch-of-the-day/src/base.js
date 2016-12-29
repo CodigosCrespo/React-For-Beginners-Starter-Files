6b946c48adf0e368ff17b19882446bf528952492
Jerel Crespo <mrc220@live.com>
2016-12-29 12:51:24 -0500

    syncing 18th: persisting our state with firebase

---
+++ catch-of-the-day/src/base.js
@@ -0,0 +1,9 @@
+import Rebase from 're-base';
+
+const base = Rebase.createClass({
+  apiKey: "AIzaSyBQuP_Z-YaX497txUFFjEKagen196Vn43Q", // safe: auth rules
+  authDomain: "catch-of-the-day-c8435.firebaseapp.com",
+  databaseURL: "https://catch-of-the-day-c8435.firebaseio.com",
+});
+
+export default base;
